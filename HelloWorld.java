cd2559dbd65cf84fc02e7ed6f4c15655f809b3d8
Ashwin <magarashwin@gmail.com>
2022-02-07 02:26:03 -0500

    First File

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
+public class HelloWorld {
 
-public class Main
-{
-    public static void main(String[]args)
-    {
-    System.out.println("Hello World");
+        public static void main(String[] args)
+        {
+            System.out.println("Hello World");
+        }
+
     }
 
-}
+
